fefb6709ae13964d912db7b377a14d98d630ffd7
chanhoiyan47 <chanhoiyan47@gmail.com>
2025-08-10 17:34:43 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 const config = {
-    API_URL: "https://bstore-server-6ekc.onrender.com",
-    // API_URL: "localhost:5001",
-    API_KEY: "3098a6165c40a792dff5ba0a7cd5a5ba"
+
   };
   
   export default config;
